5718b88952d4e81460707c1eaf77bfc9205da3a6
Marco <cd.marco.0106@gmail.com>
2021-05-21 18:01:49 -0500

    Creacion de seguimiento orden

--- Backend/views/modulos/segimientoOrden.php
+++ Backend/views/modulos/segimientoOrden.php
@@ -1,63 +1,376 @@
-<div class="content-wrapper"> 
-  <section class="content-header">
-    <h1>Seguimiento Ordenes Fabricación</h1>
-    <ol class="breadcrumb">
-      <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
-      <li class="active">Seguimiento Ordenes Fabricación</li>
-    </ol>
-  </section>
-  <section class="content">
-    <div class="box" style="background-color: #D9EEFA;">
-      <div class="box-header with-border">
-        <h3 class="box-title">Rastreabilidad por numero de parte</h3>
-        <div class="box-tools pull-right">
-          <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
-            <i class="fa fa-minus"></i></button>
-          <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-            <i class="fa fa-times"></i></button>
+  <div class="content-wrapper"> 
+    <section class="content-header">
+      <h1>
+        Seguimiento Material
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
+        <li class="active">Administrar Productos</li>
+      </ol>
+    </section>
+
+    
+
+    <!-- Main content -->
+    <section class="content">
+      <ul class="nav nav-tabs">
+          <li class="active"><a data-toggle="tab" href="#tabAreas">Areas</a></li>
+          <li><a data-toggle="tab" href="#tabSegIndividual">Seguimiento Individual</a></li>
+      </ul>
+
+      <div class="tab-content">
+        <div class="tab-pane fade in active" id="tabAreas">
+          <div class="box">
+            <div class="box-header with-border">
+              <h3 class="box-title">Areas</h3>
+
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
+                        title="Collapse">
+                  <i class="fa fa-minus"></i></button>
+                <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
+                  <i class="fa fa-times"></i></button>
+              </div>
+            </div>
+
+            <div class="box-body">
+              <div class="box-body">
+                <div class="row">
+                  <div class="col-lg-2 col-sm-6 col-xs-6">
+                    <div class="small-box bg-primary">
+                      <div class="inner">
+                        <a href="tablaReporMezclas" style="color: #fff">
+                        <h4 style="font-size: 25px">Preformas</h4>
+                        <p> INES</p>
+                        </a>
+                      </div>
+                      <div class="icon">
+                        <i class="ion ion-filing"></i>
+                      </div>
+                      <a href="tablaReporMezclas" class="small-box-footer"> <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                  </div>
+                  <div class="col-lg-2 col-sm-6 col-xs-6">
+                    <div class="small-box bg-olive">
+                      <div class="inner">
+                        <a href="tablaReporMezclas" style="color: #fff">
+                        <h4 style="font-size: 25px">Prensas (Precurado)</h4>
+                        <p>TAVO</p>
+                        </a>
+                      </div>
+                      <div class="icon">
+                        <i class="ion ion-filing"></i>
+                      </div>
+                      <a href="tablaReporMezclas" class="small-box-footer"> <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                  </div>
+                  <div class="col-lg-2 col-sm-6 col-xs-6">
+                    <div class="small-box bg-primary">
+                      <div class="inner">
+                        <a href="tablaReporMezclas" style="color: #fff">
+                        <h4 style="font-size: 25px">Rectificado</h4>
+
+                        <!--  -->
+                        <p>JOSH</p>
+                        </a>
+                      </div>
+                      <div class="icon">
+                        <i class="ion ion-filing"></i>
+                      </div>
+                      <a href="tablaReporMezclas" class="small-box-footer"> <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                  </div>
+                  <div class="col-lg-2 col-sm-6 col-xs-6">
+                    <div class="small-box bg-olive">
+                      <div class="inner">
+                        <a href="tablaReporMezclas" style="color: #fff">
+                        <h4 style="font-size: 25px">Pintura</h4>
+                        <p>MATY</p>
+                        </a>
+                      </div>
+                      <div class="icon">
+                        <i class="ion ion-filing"></i>
+                      </div>
+                      <a href="tablaReporMezclas" class="small-box-footer"> <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                  </div>
+                  <div class="col-lg-2 col-sm-6 col-xs-6">
+                    <div class="small-box bg-primary">
+                      <div class="inner">
+                        <a href="tablaReporMezclas" style="color: #fff">
+                        <h4 style="font-size: 25px">Codificado</h4>
+                        <p>JOSH</p>
+                        </a>
+                      </div>
+                      <div class="icon">
+                        <i class="ion ion-filing"></i>
+                      </div>
+                      <a href="tablaReporMezclas" class="small-box-footer"> <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                  </div>
+                  <div class="col-lg-2 col-sm-6 col-xs-6">
+                    <div class="small-box bg-olive">
+                      <div class="inner">
+                        <a href="tablaReporMezclas" style="color: #fff">
+                        <h4 style="font-size: 25px">Empaque</h4>
+                        <p>MATY</p>
+                        </a>
+                      </div>
+                      <div class="icon">
+                        <i class="ion ion-filing"></i>
+                      </div>
+                      <a href="tablaReporMezclas" class="small-box-footer"> <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="box-footer">Footer</div>
+          </div>
+        </div>
+
+        <div class="tab-pane fade in" id="tabSegIndividual">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="box">
+            <div class="box-header with-border">
+              <div class="row">
+                <div class="col-md-3"><h3 class="box-title">Seguimiento Individual</h3></div>
+                <div class="col-md-3">
+                   <div class="form-group">
+                      <label>Identificador</label>
+                      <div class="input-group">
+                          <input type="text" class="form-control" name="">
+                      </div>
+                    </div>                  
+                </div>
+              </div>
+              
+
+             
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
+                        title="Collapse">
+                  <i class="fa fa-minus"></i></button>
+                <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
+                  <i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            <div class="box-body">
+              <section class="content-header">
+                <h1>
+                  C31793    -     CCM8397-1281-I
+                  <small>Orden Fabricacion</small>
+                </h1>
+              </section>
+              <section class="content">
+                <div class="row">
+                  <div class="col-md-12">
+                    <ul class="timeline">
+                      <li class="time-label">
+                        <span class="bg-green">
+                          2019-02-15
+                        </span>
+                      </li>
+                      <li>
+                        <i class="fa fa-balance-scale bg-green"></i>
+
+                        <div class="timeline-item">
+                          <span class="time"><i class="fa fa-clock-o"></i> Fecha Elaboracion 2019-02-14</span>
+
+                          <h3 class="timeline-header"><a href="#">Area</a> Mezclas</h3>
+
+                          <div class="timeline-body">
+                            <dl class="dl-horizontal">
+                              <dt>Tipo de Formula</dt>
+                                <dd>CERAMIC LP V-11</dd>
+                              <dt>Mezclo</dt>
+                                <dd>834</dd>
+                              <dt>Lote</dt>
+                                <dd>306-R</dd>
+                              <dt>Con Recuperacion</dt>
+                                <dd>si y se agregaron <kbd>2</kbd> Cubetas de 19 Lts.</dd>
+                            </dl>
+                          </div>
+                          <div class="timeline-footer">
+                            <a class="btn btn-primary btn-xs">Ver tarjeta de Mezclas</a>
+                          </div>
+                        </div>
+                      </li>
+                      <li>
+                        <i class="fa fa-comments bg-blue"></i>
+
+                        <div class="timeline-item">
+                          <span class="time"><i class="fa fa-clock-o"></i> Fecha Elaboracion 2019-02-15</span>
+
+                          <h3 class="timeline-header"><a href="#">Area</a> Preforma</h3>
+
+                          <div class="timeline-body">
+                           <dl class="dl-horizontal">
+                              <dt># Preforma</dt>
+                                <dd><kbd>4</kbd></dd>
+                              <dt>Ubicacion en Mueble</dt>
+                                <dd>H-6</dd>
+                              <dt>Registrado por</dt>
+                                <dd>653</dd>
+                              <dt>Piezas Fabricadas</dt>
+                                <dd>425</dd>
+                              <dt>Peso Real gr.</dt>
+                                <dd>100</dd>
+                            </dl>
+                          </div>
+                          <div class="timeline-footer">
+                            <a class="btn btn-primary btn-xs">Ver tarjeta de Preformas</a>
+                          </div>
+                        </div>
+                      </li>
+                      <li>
+                        <i class="fa fa-comments bg-blue"></i>
+
+                        <div class="timeline-item">
+                          <span class="time"><i class="fa fa-clock-o"></i> Fecha Elaboracion 2019-02-22</span>
+
+                          <h3 class="timeline-header"><a href="#">Area</a> Preforma</h3>
+
+                          <div class="timeline-body">
+                           <dl class="dl-horizontal">
+                              <dt>Turno</dt>
+                                <dd>2</dd>
+                              <dt>Registrado por</dt>
+                                <dd>949</dd>
+                              <dt># Prensa de Curado</dt>
+                                <dd><kbd>7</kbd></dd>
+                              <dt>Cavidades Usadas</dt>
+                                <dd><kbd>7</kbd></dd>
+                              <dt>#Canastilla</dt>
+                                <dd>113</dd>
+                              <dt>Piezas Fabricadas</dt>
+                                <dd>425</dd>
+                              <dt>Peso Real gr.</dt>
+                                <dd>100</dd>
+                            </dl>
+                          </div>
+                          <div class="timeline-footer">
+                            <a class="btn btn-primary btn-xs">Ver tarjeta de Mezclas</a>
+                          </div>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </section>
+              
+            </div>
+            <div class="box-footer">Footer</div>
+          </div>
+            </div>
+            <div class="col-md-6">
+              <div class="box">
+            <div class="box-header with-border">
+              <div class="row">
+                <div class="col-md-3"><h3 class="box-title">Seguimiento Individual</h3></div>
+                <div class="col-md-3">
+                   <div class="form-group">
+                      <label>Identificador</label>
+                      <div class="input-group">
+                          <input type="text" class="form-control" name="">
+                      </div>
+                    </div>                  
+                </div>
+              </div>
+              
+
+             
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
+                        title="Collapse">
+                  <i class="fa fa-minus"></i></button>
+                <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
+                  <i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            <div class="box-body">
+              <section class="content-header">
+                <h1>
+                  C31793    -     CCM8397-1281-O
+                  <small>Orden Fabricacion</small>
+                </h1>
+              </section>
+              <section class="content">
+                <div class="row">
+                  <div class="col-md-12">
+                    <ul class="timeline">
+                      <li class="time-label">
+                        <span class="bg-green">
+                          2019-02-15
+                        </span>
+                      </li>
+                      <li>
+                        <i class="fa fa-balance-scale bg-green"></i>
+
+                        <div class="timeline-item">
+                          <span class="time"><i class="fa fa-clock-o"></i> Fecha Elaboracion 2019-02-14</span>
+
+                          <h3 class="timeline-header"><a href="#">Area</a> Mezclas</h3>
+
+                          <div class="timeline-body">
+                            <dl class="dl-horizontal">
+                              <dt>Tipo de Formula</dt>
+                                <dd>CERAMIC LP V-11</dd>
+                              <dt>Mezclo</dt>
+                                <dd>834</dd>
+                              <dt>Lote</dt>
+                                <dd>306-R</dd>
+                              <dt>Con Recuperacion</dt>
+                                <dd>si y se agregaron <kbd>2</kbd> Cubetas de 19 Lts.</dd>
+                            </dl>
+                          </div>
+                          <div class="timeline-footer">
+                            <a class="btn btn-primary btn-xs">Ver tarjeta de Mezclas</a>
+                          </div>
+                        </div>
+                      </li>
+                      <li>
+                        <i class="fa fa-comments bg-blue"></i>
+
+                        <div class="timeline-item">
+                          <span class="time"><i class="fa fa-clock-o"></i> Fecha Elaboracion 2019-02-15</span>
+
+                          <h3 class="timeline-header"><a href="#">Area</a> Preforma</h3>
+
+                          <div class="timeline-body">
+                           <dl class="dl-horizontal">
+                              <dt># Preforma</dt>
+                                <dd><kbd>4</kbd></dd>
+                              <dt>Ubicacion en Mueble</dt>
+                                <dd>H-6</dd>
+                              <dt>Registrado por</dt>
+                                <dd>653</dd>
+                              <dt>Piezas Fabricadas</dt>
+                                <dd>425</dd>
+                              <dt>Peso Real gr.</dt>
+                                <dd>100</dd>
+                            </dl>
+                          </div>
+                          <div class="timeline-footer">
+                            <a class="btn btn-primary btn-xs">Ver tarjeta de Mezclas</a>
+                          </div>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </section>
+              
+            </div>
+            <div class="box-footer">Footer</div>
+          </div>
+            </div>
+          </div>
+          
         </div>
       </div>
-    <div class="box-body">
-    	<ul class="timeline">
-		    <!-- timeline time label -->
-		    <li class="time-label">
-	        	<span class="bg-red">
-	            10 Feb. 2014
-	        	</span>
-    		</li>
-    		<!-- /.timeline-label -->
-    		<!-- timeline item -->
-    		<li>
-		        <!-- timeline icon -->
-		        <i class="fa fa-envelope bg-blue"></i>
-        		<div class="timeline-item">
-	            	<span class="time"><i class="fa fa-clock-o"></i> 12:05</span>
-	            	<h3 class="timeline-header"><a href="#">Support Team</a> ...</h3>
-		            <div class="timeline-body">
-		                ...
-		                Content goes here
-		            </div>
-	            	<div class="timeline-footer">
-	                	<a class="btn btn-primary btn-xs">...</a>
-	            	</div>
-        		</div>
-    		</li>
-        <li>
-          <i class="fa fa-user bg-aqua"></i>
-          <div class="timeline-item">
-            <span class="time"><i class="fa fa-clock-o">5 mins ago</i></span>
-            <h3 class="timeline-header no-border"><a href="#">Sarah Young</a> accepted your friend request</h3>
-          </div>
-        </li>
-    		<!-- END timeline item -->
-    		<li>
-    			<i class="fa fa-clock-o bg-gray"></i>
-    		</li>
-		</ul>
-    </div>
-    <div class="box-footer">
-          Footer
-    </div>
-    </div>
-  </section>
-</div>
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
